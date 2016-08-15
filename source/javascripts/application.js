3bf90b67d3d7006268f6551e84360bde5580743a
Jeferson Farias Calazans <calazans10@gmail.com>
2016-08-14 22:44:08 -0300

    WIP

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,14 @@
 //= require_tree .
+
+function toggleMenu(event) {
+  event.preventDefault();
+  document.querySelector('body').classList.toggle('overlay');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elements = document.querySelectorAll('a[data-toggle]');
+
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", toggleMenu, false);
+  }
+});
