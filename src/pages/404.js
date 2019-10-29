cdc2df3a4301e41da880fd2032e6b7e88e1567d2
Jeferson Farias Calazans <calazans10@gmail.com>
2019-10-29 08:11:41 -0300

    Update content from page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,15 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404 - Página não encontrada." />
+    <div>
+      <h1>Erro 404</h1>
+      <h2>Página não encontrada</h2>
+      <p>
+        Desculpe, a página que você está procurando não existe ou não está
+        disponível.
+      </p>
+    </div>
   </Layout>
 )
 
